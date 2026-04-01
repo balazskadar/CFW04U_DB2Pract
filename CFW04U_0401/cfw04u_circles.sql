f8b1fbaf6259d4f6202aec2e8f3607856e3a9e40
balazskadar <balazs.kadar@student.uni-miskolc.hu>
2026-04-01 14:21:17 +0200

    .

--- CFW04U_0401/cfw04u_circles.sql
+++ CFW04U_0401/cfw04u_circles.sql
@@ -1,13 +1,51 @@
 
 CREATE TABLE Circles (
-    RADIUS NUMBER(4, 0),
-    CIRCUMFERENCE NUMBER(22),
-    AREA NUMBER(22),
-
-    CONSTRAINT pk_circles PRIMARY KEY (RADIUS)
+    RADIUS NUMBER(4) PRIMARY KEY,
+    CIRCUMFERENCE NUMBER,
+    AREA NUMBER
 );
 
-CREATE OR REPLACE FUNCTION PI RETURN NUMBER IS
+DESC cIRCLES
+
+select 1 Radius, 2*1*PI() CIRCUMFERENCE from dual;
+
+//kör kerülete
+ DECLARE
+ Radius number:= 1;
+    Circumference number;
 BEGIN
-    RETURN ACOS(-1);
+    Circumference := 2*Radius*PI();
+    dbms_output.put_line('Radius: '||Radius||', Circumference: '||Circumference);
 END;
+
+DECLARE
+
+Circumference number;
+X NUMBER:= 1;
+y NUMBER:= 5;
+BEGIN
+   FOR i IN x..y LOOP
+    Circumference := 2*i*PI();
+    dbms_output.put_line('Radius: '||i||', Circumference: '||Circumference);
+   END LOOP;
+END;
+
+//több 1...5 sugarú kör kerülete eljárással
+CREATE OR REPLACE PROCEDURE Circler (x in number, y in number) IS
+Circumference number;
+BEGIN
+   FOR i IN x..y LOOP
+    Circumference := 2*i*PI();
+    dbms_output.put_line('Radius: '||i||', Circumference: '||Circumference);
+   END LOOP;
+END;
+
+BEGIN
+    Circler(1,5);
+END;
+
+
+
+
+
+    
